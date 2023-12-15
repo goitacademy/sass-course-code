05daad03344b45c8f2226709fe6b6dc77704a521
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2023-12-15 15:34:20 +0200

    init project

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,35 @@
-import * as bodyScrollLock from "body-scroll-lock";
+// Commect styles
+import '../scss/main.scss';
 
-import "../scss/main.scss";
+import * as bodyScrollLock from 'body-scroll-lock';
 
 // Sidebar menu
 const refsMenu = {
-  openMenuBtn: document.querySelector(".js-menu-open"),
-  closeMenuBtn: document.querySelector(".js-menu-close"),
-  overlayMenu: document.querySelector(".js-menu"),
+  openMenuBtn: document.querySelector('.js-menu-open'),
+  closeMenuBtn: document.querySelector('.js-menu-close'),
+  overlayMenu: document.querySelector('.js-menu'),
 };
 
 const toggleMenu = () => {
   const isMenuOpen =
-    refsMenu.openMenuBtn.getAttribute("aria-expanded") === "true" || false;
-  refsMenu.openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-  refsMenu.overlayMenu.classList.toggle("is-open");
+    refsMenu.openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+  refsMenu.openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+  refsMenu.overlayMenu.classList.toggle('is-open');
 
   const scrollLockMethod = !isMenuOpen
-    ? "disableBodyScroll"
-    : "enableBodyScroll";
+    ? 'disableBodyScroll'
+    : 'enableBodyScroll';
   bodyScrollLock[scrollLockMethod](document.body);
 };
 
-refsMenu.openMenuBtn.addEventListener("click", toggleMenu);
-refsMenu.closeMenuBtn.addEventListener("click", toggleMenu);
+refsMenu.openMenuBtn.addEventListener('click', toggleMenu);
+refsMenu.closeMenuBtn.addEventListener('click', toggleMenu);
 
 // Close the mobile menu on wider screens if the device orientation changes
-window.matchMedia("(min-width: 1200px)").addEventListener("change", (event) => {
+window.matchMedia('(min-width: 1200px)').addEventListener('change', event => {
   if (!event.matches) return;
 
-  refsMenu.overlayMenu.classList.remove("is-open");
-  refsMenu.openMenuBtn.setAttribute("aria-expanded", false);
+  refsMenu.overlayMenu.classList.remove('is-open');
+  refsMenu.openMenuBtn.setAttribute('aria-expanded', false);
   bodyScrollLock.enableBodyScroll(document.body);
 });
